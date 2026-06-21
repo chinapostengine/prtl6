04a77d9351e4d76a141d363a279392e6834de4e8
chinapostengine <chinapostengine@gmail.com>
2026-06-21 22:28:51 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e  # Exit immediately if any command fails
 # -------------------------------
 # Configuration
 # -------------------------------
-PROJECT_ID="e4wr5tgyv4er35gyb"
+PROJECT_ID="sonic-wavelet-494809-h7"
 REGION="europe-west4"
 IMAGE_NAME="pl"
 REPO_NAME="main-repo"

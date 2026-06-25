83b004052e42e780b079c0de40a8e3e8f6cdc24c
chinapostengine <chinapostengine@gmail.com>
2026-06-25 09:41:16 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e  # Exit immediately if any command fails
 # -------------------------------
 # Configuration
 # -------------------------------
-PROJECT_ID="urkhte"
+PROJECT_ID="macro-depot-500501-q2"
 REGION="europe-west4"
 IMAGE_NAME="pl"
 REPO_NAME="main-repo"

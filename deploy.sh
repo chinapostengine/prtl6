de0075545480032c74ed13e337dd2b103eba23e7
chinapostengine <chinapostengine@gmail.com>
2026-07-05 01:13:41 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e  # Exit immediately if any command fails
 # -------------------------------
 # Configuration
 # -------------------------------
-PROJECT_ID="aliproduct-479218"
+PROJECT_ID="ornate-spring-501408-h5"
 REGION="europe-west4"
 IMAGE_NAME="pl"
 REPO_NAME="main-repo"

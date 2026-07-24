8f04426b6a1de6fb20aa39641507fa2e973def96
chinapostengine <chinapostengine@gmail.com>
2026-07-25 01:48:51 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e  # Exit immediately if any command fails
 # -------------------------------
 # Configuration
 # -------------------------------
-PROJECT_ID="my-first-projectz-501710"
+PROJECT_ID="loadin-e7f74"
 REGION="europe-west4"
 IMAGE_NAME="pl"
 REPO_NAME="main-repo"
